f66cf4037cb5b394d03ccebc410dca951b284ad8
Natanel Niazoff <nniazoff@icloud.com>
2018-01-07 12:00:28 -0500

    add tableview

--- Sammys/ItemsViewController.swift
+++ Sammys/ItemsViewController.swift
@@ -16,8 +16,16 @@ class ItemsViewController: UIViewController {
     }
     let items = ["Avocado", "Beets", "Cucumber", "Eggs", "Garlic"]
     
+    // MARK: - IBOutlets & View Properties
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var itemsLabel: UILabel!
+    @IBOutlet weak var itemStackView: UIStackView!
     @IBOutlet weak var itemLabel: UILabel!
+    
+    var tableViewIsShowing = false
+    var tableViewConstraints: [NSLayoutConstraint] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +34,35 @@ class ItemsViewController: UIViewController {
             let animator = LinearCardAttributesAnimator(itemSpacing: 0.4, scaleRate: 0.75)
             layout.animator = animator
         }
+        view.sendSubview(toBack: collectionView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableViewConstraints = [
+            tableView.topAnchor.constraint(equalTo: itemsLabel.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ]
         
         itemLabel.text = items[currentIndex]
+    }
+    
+    @IBAction func showTableView(_ sender: UIButton) {
+        if tableViewIsShowing {
+            tableView.removeFromSuperview()
+            tableViewConstraints.forEach { $0.isActive = false }
+            
+            collectionView.isHidden = false
+            itemStackView.isHidden = false
+            tableViewIsShowing = false
+        } else {
+            view.addSubview(tableView)
+            tableViewConstraints.forEach { $0.isActive = true }
+            
+            collectionView.isHidden = true
+            itemStackView.isHidden = true
+            tableViewIsShowing = true
+        }
     }
 }
 
@@ -43,6 +78,22 @@ extension ItemsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath)
         cell.layer.cornerRadius = 20
+        return cell
+    }
+}
+
+extension ItemsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
         return cell
     }
 }
