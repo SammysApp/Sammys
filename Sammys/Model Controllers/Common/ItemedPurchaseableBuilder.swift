2f328349944fff0cdf1ec17b43ed1a3e77ad9044
Natanel Niazoff <nniazoff@icloud.com>
2018-11-11 20:46:46 -0500

    Add toggleExisting

--- Sammys/Model Controllers/Common/ItemedPurchaseableBuilder.swift
+++ Sammys/Model Controllers/Common/ItemedPurchaseableBuilder.swift
@@ -14,6 +14,16 @@ protocol ItemedPurchaseableBuildable {
 
 protocol ItemedPurchaseableBuilder {
 	mutating func toggle(_ item: Item, with modifier: Modifier?) throws
+	mutating func toggleExisting(in itemedPurchaseable: ItemedPurchaseable) throws
 	func build() throws -> ItemedPurchaseable
 	init()
+}
+
+extension ItemedPurchaseableBuilder {
+	mutating func toggleExisting(in itemedPurchaseable: ItemedPurchaseable) throws {
+		for category in type(of: itemedPurchaseable).allItemCategories {
+			// FIXME: Handle modifiers
+			try itemedPurchaseable.items(for: category).forEach { try toggle($0, with: nil) }
+		}
+	}
 }
