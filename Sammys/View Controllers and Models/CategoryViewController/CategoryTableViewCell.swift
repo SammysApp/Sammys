e96ed8766c0d866603696a9c6b8d43cfebf67fc8
Natanel Niazoff <nniazoff@icloud.com>
2018-12-11 13:42:34 -0500

    Add CategoryViewController

---
+++ Sammys/View Controllers and Models/CategoryViewController/CategoryTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryTableViewCell.swift
+//  Sammys
+//
+//  Created by Natanel Niazoff on 12/11/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+	@IBOutlet var titleLabel: UILabel!
+}
---
+++ Sammys/View Controllers and Models/CategoryViewController/CategoryViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CategoryViewModel.swift
+//  Sammys
+//
+//  Created by Natanel Niazoff on 12/11/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import Foundation
